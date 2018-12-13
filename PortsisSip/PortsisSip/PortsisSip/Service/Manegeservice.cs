4bd5c66bc0a544873d8b4b3a7c767791a36a53b1
sabriozay <36267741+sabriozay@users.noreply.github.com>
2018-12-13 13:19:52 +0300

    sabri
    türkce karakter sorunu düzeldi

--- PortsisSip/PortsisSip/PortsisSip/Service/Manegeservice.cs
+++ PortsisSip/PortsisSip/PortsisSip/Service/Manegeservice.cs
@@ -2,6 +2,8 @@
 using PortsisSip.Model;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,13 +22,19 @@ namespace PortsisSip.Service
 
         public async void GetMaintOrderListRoot(müsteripost mst)
         {
-         
-  var input = "http://onlinesiparis.portsispos.com/pos.asmx/SipAl?Adsoyad="+mst.AdSoyad+"&Telefon="+mst.Telefon+"&Adres="+mst.Adres+ "&Aciklama="+ "OnlineSip" +"&kod=" + mst.Kod+"";
+            mst.AdSoyad = degis(mst.AdSoyad);
+            mst.Adres = degis(mst.Adres);
+            var input = "http://onlinesiparis.portsispos.com/pos.asmx/SipAl?Adsoyad="+mst.AdSoyad+"&Telefon="+mst.Telefon+"&Adres="+mst.Adres+ "&Aciklama="+ "OnlineSip" +"&kod=" + mst.Kod+"";
        //     var input = "http://onlinesiparis.portsispos.com/pos.asmx/SipAl?Adsoyad=sabri&Telefon=234&Adres=asd&Aciklama=asdasd&kod=reyelhali";
             var client = await GetClient();
             var result = await client.GetStringAsync(input);
            
         }
+        private string degis(string gelen)
+        {
+            return String.Join("", gelen.Normalize(NormalizationForm.FormD).Where(c => char.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark));
+        }
+
 
 
     }
